fbbe619d3a2bb5171f7779b85131c3aa11825730
HappyZ <zhuyanzi@gmail.com>
2018-03-27 10:41:48 -0500

    add built lib

---
+++ android_tshark_lib/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/yanzi/.android_lib/lib"
+XML2_LIBS="-lxml2 -lz   -L/home/yanzi/.android_lib/lib -llzma     -liconv  -lm -lc"
+XML2_INCLUDEDIR="-I/home/yanzi/.android_lib/include/libxml2"
+MODULE_VERSION="xml2-2.9.8"
+
